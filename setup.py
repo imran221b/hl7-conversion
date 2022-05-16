8ab163f5bf674418d66fb0611d5526d7e8fe7d68
Imran Hasan <imran221b@gmail.com>
2022-05-16 18:14:57 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from setuptools import find_packages, setup
+import pathlib
+
+HERE = pathlib.Path(__file__).resolve().parent
+README = (HERE / "README.md").read_text()
 
 setup(
     name="hl7-hl7_convert",
-    version="0.1.1",
+    version="0.1.0",
     author="Imran Hasan",
     author_email="imran221b@gmail.com",
     description="Package to hl7_convert HL7 messages to JSON format",
-    url="",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/imran221b/hl7-conversion",
     packages=find_packages(exclude=["docs", "tests"]),
     platforms=["posix"],
     entry_points={
